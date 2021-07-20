a8e338a4940fd81b17ac5f5462daffec7d36dad8
mounikakanda <87506316+mounikakanda@users.noreply.github.com>
2021-07-20 20:33:36 +0530

    mounika commit

--- sample_code.sh
+++ sample_code.sh
@@ -5,5 +5,5 @@ echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
-SUM=$((FIRSTNUMBER + SECONDNUMBER+Thirdnumber))
+SUM=$((FIRSTNUMBER + SECONDNUMBER+Thirdnumber+fourth number))
 echo "$SUM"
